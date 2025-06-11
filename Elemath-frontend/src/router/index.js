7e36bb00c60c6ceca9e136cf6abb7a6c59ea2277
JamesPatrick30 <mrsniper1388@gmail.com>
2025-06-11 15:05:06 +0800

    setting up the frontend

--- Elemath-frontend/src/router/index.js
+++ Elemath-frontend/src/router/index.js
@@ -1,22 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import landingpage from '@/views/landingpage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'landingpage',
+      component: landingpage,
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+
   ],
 })
 
---
+++ Elemath-frontend/src/views/landingpage.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <section class="landing">
+      hello
+      <!-- <div class="iem1"></div>
+      <div class="iem2"></div>
+      <div class="iem3"></div> -->
+    </section>
+
+    <section class="about">
+      hello
+      <!-- <div class="iem1"></div>
+      <div class="iem2"></div>
+      <div class="iem3"></div> -->
+    </section>
+  </main>
+</template>
+
+<style scoped>
+
+body {
+  margin: 0;
+  padding: 0;
+}
+.landing {
+  background-color: aqua;
+  height: 100vh;
+  width: 100%;
+}
+
+.about {
+  background-color: blue;
+  height: 100vh;
+  width: 100%;
+}
+</style>
