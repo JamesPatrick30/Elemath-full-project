6f941395ae86161176bf132dc4cd9a79d3b257cc
JamesPatrick30 <mrsniper1388@gmail.com>
2025-06-18 12:17:33 +0800

    add image and background

---
+++ Elemath-frontend/src/components/svg/bluebackground.vue
@@ -0,0 +1,21 @@
+<template>
+    <svg viewBox="0 0 1511 364" width="100%" height="auto" preserveAspectRatio="none">
+                  <defs>
+                    <linearGradient id="beachGradient" x1="0" y1="0" x2="0" y2="1">
+                      <stop offset="0%" stop-color="#A2F2FF" />
+                      <stop offset="100%" stop-color="#47C7FF" />
+                    </linearGradient>
+                  </defs>
+
+                  <path
+                    d="M0,50
+                      C 150,20 400,80 600,60 
+                      C 800,40 1000,70 1200,50 
+                      C 1350,40 1450,80 1511,50 
+                      L 1511,364 
+                      L 0,364 
+                      Z"
+                    fill="url(#beachGradient)"
+                  />
+                </svg>
+</template>
