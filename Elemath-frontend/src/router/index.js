1582a8d43d1083c11fa37776ce7a527533194f04
JamesPatrick30 <mrsniper1388@gmail.com>
2025-06-26 12:43:29 +0800

    added in the router

--- Elemath-frontend/src/router/index.js
+++ Elemath-frontend/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import landingpage from '@/views/landingpage.vue'
 import signin from '@/views/SignIn.vue'
 import SignUp from '@/views/SignUp.vue'
-
+import studentDashboard from '@/views/studentDashboard.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -20,6 +20,11 @@ const router = createRouter({
       path: '/signup',
       name: 'signup',
       component: SignUp
+    },
+    {
+      path: '/student-dashboard',
+      name: 'studentDashboard',
+      component: studentDashboard
     }
 
   ],
