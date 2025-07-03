d44dea3c51496594c88cb444f053d53f10931d64
JamesPatrick30 <mrsniper1388@gmail.com>
2025-07-03 16:27:34 +0800

    add teacher dashboard

--- Elemath-frontend/src/router/index.js
+++ Elemath-frontend/src/router/index.js
@@ -3,6 +3,8 @@ import landingpage from '@/views/landingpage.vue'
 import signin from '@/views/SignIn.vue'
 import SignUp from '@/views/SignUp.vue'
 import studentDashboard from '@/views/studentDashboard.vue'
+
+import teacberDashboard from '@/views/teacher-ui/teacber-dashboard.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -25,6 +27,11 @@ const router = createRouter({
       path: '/student-dashboard',
       name: 'studentDashboard',
       component: studentDashboard
+    },
+    {
+      path: '/t',
+      name: 'teacher-dashboard',
+      component: teacberDashboard
     }
 
   ],
---
+++ Elemath-frontend/src/views/teacher-ui/teacber-dashboard.vue
@@ -0,0 +1,59 @@
+<template>
+    <body class="body">
+        <nav>
+            <ul>
+                <li class="item1">Home</li>
+                <li class="item2">Profile</li>
+                <li class="item3">History</li>
+                <li class="item4">Settings</li>
+            </ul>
+        </nav>
+        <main>
+            <div class="contaner">
+                <div class="item1" id="item"></div>
+                <div class="item2" id="item"></div>
+                <div class="item3" id="item" ></div>
+                <div class="item4" id="item"></div>
+            </div>
+            
+        </main>
+    </body>
+</template>
+<style scoped>
+.body{
+    height: 100vh;
+    width: 100%;
+    background-color: #c25252;
+    display: flex;
+    flex-direction: row;
+}
+nav{
+    height: 100%;
+    background-color: antiquewhite;
+    width: 15%;
+}
+main{
+    height: 100%;
+    width: 100%;
+    background-color: aliceblue;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.contaner{
+    height: 90%;
+    width: 90%;
+    display: grid;
+    grid-template-rows: 20% 70%;
+    grid-template-columns: 2fr 3fr;
+    grid-area: 
+        'item1 item2'
+        'item3 item4';
+        gap: 30px;
+}
+#item{
+    background-color: white;
+    box-shadow: 0 4px 16px rgba(0, 0, 0, 0.15);
+    border-radius: 20px;
+}
+</style>
