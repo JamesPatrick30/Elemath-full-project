b135c290955cbbea0d7ece4993d4f1cfa6d3eef4
JamesPatrick30 <mrsniper1388@gmail.com>
2025-07-05 19:05:43 +0800

    finish tomorow

--- Elemath-frontend/src/views/teacher-ui/teacher-ui.vue
+++ Elemath-frontend/src/views/teacher-ui/teacher-ui.vue
@@ -1,3 +1,83 @@
 <template>
-    <h1>fdf</h1>
+    <body>
+        <nav>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+        </nav>
+        <main>
+            <div class="con-m">
+            <div id="item" style="grid-area: header;"></div>
+            <div class="body"  style="grid-area: body;">
+                <div class="con-b">
+                    <div id="games" style="grid-area: quizmode;">3</div>
+                    <div id="games" style="grid-area: windowcard;">4</div>
+                    <div id="games" style="grid-area: challenge;">3</div>
+                    <div id="games" style="grid-area: lesson;">6</div>
+                </div>
+                
+            </div>
+            <div id="item" style="grid-area: list;"></div>
+            <div id="item" style="grid-area: statistic;"></div>
+        </div>
+        </main>
+        
+    </body>
 </template>
+<style scoped>
+#games{
+    background-color: white;
+    border-radius: 20px;
+       box-shadow: 0 4px 16px rgba(0, 0, 0, 0.12);
+}
+.body .con-b{
+    height: 90%;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    grid-template-areas: 
+        'quizmode windowcard'
+        'challenge lesson';
+    gap: 1em;
+}
+#item{
+    background-color: white;
+    box-shadow: 0 4px 16px rgba(0, 0, 0, 0.12);
+    border-radius: 20px;
+}
+.con-m{
+    height: 90%;
+    width: 95%;
+    background-color: rgb(235, 235, 235);
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 2fr 2.5fr;
+    grid-template-areas: 
+        'header header list'
+        'body body list'
+        'body body statistic';
+    gap: 1em;
+}
+body{
+    background-color: rgb(235, 235, 235);
+    height: 100vh;
+    width: 100%;
+    display: flex;
+}
+main{
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+nav{
+    height: 100%;
+    width: 250px;
+    background-color: aqua;
+}
+</style>
