80187fe27b96514d563b86c3756dd8e089e967da
Jamespatrick30 <mrsniper1388@gmail.com>
2025-07-08 15:46:35 +0800

    the heigth is fix in 200px and add an image

--- Elemath-frontend/src/views/teacher-ui/teacher-ui.vue
+++ Elemath-frontend/src/views/teacher-ui/teacher-ui.vue
@@ -12,17 +12,27 @@
         <main>
             <div class="con-m">
             <div class="header" style="grid-area: header;">
-                <h3>welcome back. james</h3>
-                <h5>ID : 664232443</h5>
+                <div class="image-contaner">
+                    <img src="/images/frog2.png" alt="">
+                </div>
+                
+                <div class="text-area">
+                    <h3>welcome back. james</h3>
+                    <h5>ID : 664232443</h5>
+                </div>
+                
             </div>
             <div class="body"  style="grid-area: body;">
                 <div class="con-b">
-                    <div class="quiz" style="grid-area: quizmode;">
+                    <div id="mode" class="quiz" style="grid-area: quizmode;">
                         <h1>QUIZ MODE</h1>
                     </div>
-                    <div class="windowcard" style="grid-area: windowcard;">4</div>
-                    <div class="challenge" style="grid-area: challenge;">3</div>
-                    <div class="lesson" style="grid-area: lesson;">6</div>
+                    <div id="mode" class="windowcard" style="grid-area: windowcard;">
+                        <h1>windowcard</h1>
+                        <img class="teach" src="/images/teach.png" alt="">
+                    </div>
+                    <div id="mode" class="challenge" style="grid-area: challenge;">3</div>
+                    <div id="mode" class="lesson" style="grid-area: lesson;">6</div>
                 </div>
                 
             </div>
@@ -37,7 +47,41 @@
 *{
     font-family: 'BubbleBody Neue','Poppins', sans-serif;
 }
+.teach{
+    position: relative;
+    left: 150px;
+    bottom: 100px;
+    height: 250px;
+    width: auto;
+}
+#mode{
+    height: 200px;
+}
+#mode:hover{
+    background:100%;
+    cursor: pointer;
+    transition: 0.1s linear;
+}
+.image-contaner{
+    display: flex;
+    align-items: center;
+    justify-content: end;
+    background-color: white;
+    border-radius: 50%;
+
+    height: fit-content;
+    width: fit-content;
+}
+.image-contaner img{
+    border-radius: 50%;
+    height: 100px;
+}
 .header{
+    display: flex;
+    justify-content: baseline;
+    align-items: center;
+
+    gap: 20px;
     color: white;
     padding: 20px;
     background-color: #fbaaa6;
@@ -49,30 +93,35 @@
     padding: 20px;
     border-radius: 20px;
     box-shadow: 0 4px 16px rgba(0, 0, 0, 0.12);
+
+    transition: 0.1s linear;
 }
 .challenge{
     background-color: #e7bb41;
     padding: 20px;
     border-radius: 20px;
     box-shadow: 0 4px 16px rgba(0, 0, 0, 0.12);
+
+    transition: 0.1s linear;
 }
 .quiz{
+    color: white;
     background-color: #2a713d;
     padding: 20px;
     border-radius: 20px;
     box-shadow: 0 4px 16px rgba(0, 0, 0, 0.12);
+
+    transition: 0.1s linear;
 }
 .windowcard{
     padding: 20px;
     background-color: #2d8bba;
     border-radius: 20px;
        box-shadow: 0 4px 16px rgba(0, 0, 0, 0.12);
-}
-#games{
 
-    border-radius: 20px;
-       box-shadow: 0 4px 16px rgba(0, 0, 0, 0.12);
+    transition: 0.1s linear;
 }
+
 .body .con-b{
     height: 90%;
     display: grid;
