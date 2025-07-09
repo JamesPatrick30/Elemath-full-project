61cd06447af9ab1a3a6eeed1ff4e620cbb2c93f7
Jamespatrick30 <mrsniper1388@gmail.com>
2025-07-09 13:55:25 +0800

    install apex chart

--- Elemath-frontend/src/main.js
+++ Elemath-frontend/src/main.js
@@ -4,9 +4,10 @@ import './assets/fonts.css';
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueApexCharts from "vue3-apexcharts";
 
 const app = createApp(App)
-
+app.use(VueApexCharts);
 app.use(router)
 
 app.mount('#app')
