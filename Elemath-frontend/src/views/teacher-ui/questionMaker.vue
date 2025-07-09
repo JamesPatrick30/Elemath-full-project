19dab1498b3d5b87d68c3b629ef72dfecd9b6874
Jamespatrick30 <mrsniper1388@gmail.com>
2025-07-09 23:11:57 +0800

    add simple ui

--- Elemath-frontend/src/views/teacher-ui/questionMaker.vue
+++ Elemath-frontend/src/views/teacher-ui/questionMaker.vue
@@ -1,3 +1,88 @@
 <template>
-    <h1>hello</h1>
+    <main>
+        <div class="con-q">
+            <nav>
+                <ul>
+                    <li><button class="nav-btn" @click="navClick('Settings')" :class="btnActive.setting? 'activeBtn' : 'not-active'">Settings</button></li>
+                    <li><button class="nav-btn" @click="navClick('Question')" :class="btnActive.Question? 'activeBtn' : 'not-active'" >Question</button></li>
+                    <li><button class="nav-btn" @click="navClick('file')" :class="btnActive.file? 'activeBtn' : 'not-active'">Upload file</button></li>
+                </ul>
+            </nav>
+        </div>
+      <div class="con-lobby">
+        <nav>
+                <ul>
+                    <li><button @click="navClick('Settings')" :class="btnActive.setting? 'activeBtn' : 'not-active'">Settings</button></li>
+                    <li><button @click="navClick('Question')" :class="btnActive.Question? 'activeBtn' : 'not-active'" >Question</button></li>
+                    <li><button @click="navClick('file')" :class="btnActive.file? 'activeBtn' : 'not-active'">Upload file</button></li>
+                </ul>
+            </nav>
+      </div>
+    </main>
+    
 </template>
+<script>
+export default{
+    data(){
+        return{
+            btnActive:{setting:true,Question:false,file:false}
+        }
+    },
+    methods:{
+        navClick(btn){
+            this.btnActive.setting = false;
+            this.btnActive.Question = false;
+            this.btnActive.file = false;
+
+            switch(btn){
+                case 'Settings':
+                    this.btnActive.setting = true;
+                break;
+                case 'Question':
+                    this.btnActive.Question = true;
+                break;
+                case 'file':
+                    this.btnActive.file = true;
+                break;
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+.nav-btn{
+    color: white;
+}
+.activeBtn{
+    border-bottom: 3px solid black;
+}
+nav ul{
+    display: flex;
+}
+li{
+    list-style: none;
+}
+button{
+    border: none;
+    background: none;
+}
+
+main{
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 60px;
+}
+.con-q,.con-lobby{
+    height: 90%;
+    width: 500px;
+    padding: 20px;
+    border-radius: 20px;
+    background-color: #a7ddea;
+}
+*{
+    font-family: 'BubbleBody Neue','Poppins', sans-serif;
+}
+</style>
