4334bd785e49ab5f0e602d16c8c6f4a34e1cd13d
Jamespatrick30 <mrsniper1388@gmail.com>
2025-07-14 15:19:05 +0800

    connect to mongodb

--- Elemath-backend-js/server.js
+++ Elemath-backend-js/server.js
@@ -3,11 +3,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const app = express();
+const http = require('http');
+const mongoose = require('mongoose');
 
 // Load environment variables from .env file
 dotenv.config();
 const PORT = process.env.PORT || 3000;
-
+const uri = process.env.MONGODB_URL;
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 
@@ -16,6 +18,15 @@ app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+const server = http.createServer(app);
+mongoose.connect(uri)
+  .then(() => {
+    server.listen(PORT,() => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+    console.log('✅ Connected to MongoDB Atlas')
+  })
+  .catch(err => console.error('❌ Connection error:', err));
 // Routes
 app.get('/', (req, res) => {
     res.send('Welcome to the Elemath Backend API');
@@ -31,6 +42,6 @@ app.post('createAccount', (req, res) => {
     const { username, password } = req.body;
 });
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
