b26ab76363b63150dc856d28afb412554b125b38
Jamespatrick30 <mrsniper1388@gmail.com>
2025-07-18 17:09:03 +0800

    install fontawesome

--- Elemath-frontend/src/views/teacher-ui/classPage.vue
+++ Elemath-frontend/src/views/teacher-ui/classPage.vue
@@ -10,6 +10,31 @@
                     <option value="">asdsd</option>
                 </select>
             </header>
+            <div class="student-list">
+                <div class="student-list-con">
+                    <table class="student-list-table">
+                        <thead>
+                            <tr>
+                                <th>firstName</th>
+                                <th>MiddleName</th>
+                                <th>LastName</th>
+                                <th>LRN</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="student in students" :key="student.lrn">
+                                <td>{{ student.firstName }}</td>
+                                <td>{{ student.middleName }}</td>
+                                <td>{{ student.lastName }}</td>
+                                <td>{{ student.lrn }}</td>
+                                <td><button><font-awesome-icon :icon="['fas', 'user-pen']" />q</button> <button>delete</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                
+            </div>
         </main>
     </body>
     
@@ -30,11 +55,46 @@ export default {
             infoName: '',
             infoMiddleName: '',
             infoLastName: '',
+            students: [
+                { firstName: 'John Doe',middleName: 'A.', lastName: 'Smith', lrn: '1234567890' },
+                { firstName: 'Jane Doe', middleName: 'B.', lastName: 'Johnson', lrn: '0987654321' },
+                { firstName: 'Alice', middleName: 'C.', lastName: 'Brown', lrn: '1122334455' },
+                { firstName: 'Bob', middleName: 'D.', lastName: 'Davis', lrn: '5566778899' },
+                
+            ],
         };
     },
 }
 </script>
 <style scoped>
+.student-list{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+}
+.student-list-table thead {
+  background-color: #f5f5f5;
+}
+.student-list-con .student-list-table td{
+    padding-top: 10px;
+    text-align: center;
+    border-bottom: 1px solid #ccc;
+}
+.student-list-con{
+    border-radius: 10px;
+    background-color: white;
+    width: 90%;
+    height: 90%;
+    display: flex;
+    justify-content: center;
+}
+.student-list-con .student-list-table{
+    width: 100%;
+    height: fit-content;
+    border-collapse: collapse;
+    gap: 10px;
+}
 *{
     font-family: 'BubbleBody Neue','Poppins', sans-serif;
 }
