b4e165fadf07c8a816a3a7fa1b735d6ff72f3246
Jamespatrick30 <mrsniper1388@gmail.com>
2025-07-28 16:23:53 +0800

    remove the other console.log

--- Elemath-backend-js/server.js
+++ Elemath-backend-js/server.js
@@ -183,7 +183,6 @@ app.get('/data/teacher', auth, async (req, res) => {
       return res.status(404).json({ message: 'User not found' });
     }
     res.status(200).json(user);
-    console.log('/data/teacher :', user);
   } catch (error) {
     console.error('Error fetching teacher data:', error);
     res.status(500).json({ message: 'Internal server error' });
