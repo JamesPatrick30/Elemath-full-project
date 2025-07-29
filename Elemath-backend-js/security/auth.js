6065d4b7f58f6ddb53182a0493b93641f0c7a6da
Jamespatrick30 <mrsniper1388@gmail.com>
2025-07-29 11:00:45 +0800

    refresh token is auto rotate in server side

--- Elemath-backend-js/security/auth.js
+++ Elemath-backend-js/security/auth.js
@@ -2,9 +2,20 @@ const jwt = require('jsonwebtoken');
 
 function auth(req, res, next) {
   const token = req.cookies.access_token;
+  const refreshtoken = req.refreshToken;
   console.log("Refresh token request cookies:", req.refreshToken);
   console.log("Refresh token request access_token:", token);
   if (!token) {
+    if (refreshtoken){
+      try {
+        const decoded = jwt.verify(refreshtoken, process.env.JWT_SECRET);
+        req.user = decoded; // ✅ Attach user data to req.user
+        console.log("✅ Authenticated decoded:", decoded);
+        next();
+      } catch (err) {
+        return res.status(401).json({ message: 'Invalid or expired token.' });
+      }
+    }
     return res.status(401).json({ message: 'Access denied. No token provided.' });
   }
 
