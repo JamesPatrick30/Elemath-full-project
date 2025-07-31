a0908af57111b1b314ae079afdd2a77f85a57506
Jamespatrick30 <mrsniper1388@gmail.com>
2025-07-31 12:42:11 +0800

    edit class

--- Elemath-backend-js/models/grade.js
+++ Elemath-backend-js/models/grade.js
@@ -1,29 +1,40 @@
 // models/Gradebook.js
 const mongoose = require('mongoose');
+// const student = require('./student');
 
+const studentSchema = new mongoose.Schema({
+  lrn:String,
+  name: String,
+  score: Number,
+  average:Number
+}, { _id: false });
+const questionSchema = new mongoose.Schema({
+  number: String,
+  question:String,
+  answer:{type: mongoose.Schema.Types.Mixed},
+  choices: {
+    type: [mongoose.Schema.Types.Mixed],
+    default:[]
+  }
+}, { _id: false });
+const quizSchema = new mongoose.Schema({
+  quizId:String,
+  quizname: String,
+  total:Number,
+  students: [studentSchema],
+  totalAverage:Number,
+  lowAnalysis:[{ type: mongoose.Schema.Types.Mixed }],
+  questions:[questionSchema]
+}, { _id: false });
+const studentListSchema = new mongoose.Schema({
+  lrn:String,
+  name:String
+}, { _id: false });
 const gradebookSchema = new mongoose.Schema({
-  studentId:     { type: mongoose.Schema.Types.ObjectId, ref: 'Student' },
   classId:       { type: String, ref: 'Class' },
   gradingPeriod: { type: String }, // e.g., Q1, Q2, Final
-  grades: {
-    quizzes: [
-      { quizId: { type: mongoose.Schema.Types.ObjectId, ref: 'Quiz' }, percentage: Number }
-    ],
-    manual: [
-      { label: String, score: Number, total: Number }
-    ]
-  },
-  weights: {
-    quiz:   { type: Number, default: 0.6 },
-    manual: { type: Number, default: 0.4 }
-  },
-  computed: {
-    quizAverage:   Number,
-    manualAverage: Number,
-    finalGrade:    Number,
-    status:        { type: String, enum: ['pass', 'fail'] }
-  },
-  updatedAt: { type: Date, default: Date.now }
+  students:[studentListSchema],
+  grades: [quizSchema]
 });
 
 module.exports = mongoose.model('Gradebook', gradebookSchema);
