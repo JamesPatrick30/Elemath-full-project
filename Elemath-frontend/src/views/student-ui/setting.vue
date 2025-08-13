98b6a8c9499d5b8ebba75a95c90031d301540bf7
Jamespatrick30 <mrsniper1388@gmail.com>
2025-08-13 16:11:56 +0800

    fix the problem

---
+++ Elemath-frontend/src/views/student-ui/setting.vue
@@ -0,0 +1,29 @@
+<template>
+    <body>
+        <navBarStudent></navBarStudent>
+        Settings
+    </body>
+    
+</template>
+<script>
+import navBarStudent from './components/navBarStudent.vue';
+export default {
+    components:{
+        navBarStudent
+    },
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<style scoped>
+body{
+    display: flex;
+    flex-direction: row;
+    height: 100vh;
+    width: 100vw;
+    background-color: #f0f0f0;
+}
+</style>
