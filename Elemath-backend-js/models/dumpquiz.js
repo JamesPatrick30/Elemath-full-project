1c27e977ed79e5de4a610f11e7e1e4e7833aebe6
Jamespatrick30 <mrsniper1388@gmail.com>
2025-08-19 18:29:41 +0800

    add dump quiz model

---
+++ Elemath-backend-js/models/dumpquiz.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const playerSchema = new mongoose.Schema({
+  playerId: String,
+  lrn : String,
+  profile:String
+});
+const dumpQuizSchema = new mongoose.Schema({
+    quizId: String,
+    quizName: {type:String,default:""},
+    quizTime: {type:Number, default: 0},
+    players:[playerSchema]
+});
+module.exports = mongoose.model('dumpQuiz', dumpQuizSchema);
