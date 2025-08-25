6cf9a0b78e49601f66ca4af8a67d1431761bf07c
Jamespatrick30 <mrsniper1388@gmail.com>
2025-08-25 23:49:11 +0800

    add /

--- Elemath-frontend/src/router/index.js
+++ Elemath-frontend/src/router/index.js
@@ -24,7 +24,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path:'rev',
+      path:'/rev',
       name:'rev',
       component:donegame
     },
