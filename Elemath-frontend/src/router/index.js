c99af88a0c29ed96f098ddadca53c3888547d1d7
Jamespatrick30 <mrsniper1388@gmail.com>
2025-08-29 00:08:17 +0800

    add setting in student

--- Elemath-frontend/src/router/index.js
+++ Elemath-frontend/src/router/index.js
@@ -8,8 +8,8 @@ import studentSetting from '@/views/student-ui/setting.vue'
 import testarea from '@/views/student-ui/game/testarea.vue'
 import donegame from '@/views/student-ui/game/donegame.vue'
 import dashboard2 from '@/views/student-ui/dashboard2.vue'
-
-import setting from '@/views/teacher-ui/setting.vue'
+import setting from '@/views/student-ui/settings.vue'
+// import setting from '@/views/teacher-ui/setting.vue'
 import teacberDashboard from '@/views/teacher-ui/teacber-dashboard.vue'
 import teacherCreateClass from '@/views/teacher-ui/teacher-createClass.vue'
 import teacherUi from '@/views/teacher-ui/teacher-ui.vue'
@@ -24,6 +24,7 @@ import leaderboard from '@/views/teacher-ui/game/leaderboard.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+
     {
       path:'/ds',
       name:'dash2',
---
+++ Elemath-frontend/src/views/student-ui/settings.vue
@@ -0,0 +1,8 @@
+<script>
+
+</script>
+<template>
+    <h1>hello</h1>
+</template>
+<style scoped>
+</style>
