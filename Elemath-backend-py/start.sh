11d1597443990c345b00e5c44f92097648d09a28
Jamespatrick30 <mrsniper1388@gmail.com>
2025-09-22 19:33:49 +0800

    done maintenance in py

--- Elemath-backend-py/start.sh
+++ Elemath-backend-py/start.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+# Activate virtual environment
 source /root/Elemath-full-project/Elemath-backend-py/venv/bin/activate
-uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+
+# Go to project directory
+cd /root/Elemath-full-project/Elemath-backend-py
+
+# Run Uvicorn in production mode (no --reload)
+uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4
