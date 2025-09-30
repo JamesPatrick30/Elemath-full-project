9954da044679a0d06964a39c299de4852b635524
JamesPatrick30 <mrsniper1388@gmail.com>
2025-09-30 07:52:22 +0000

    hehe

--- Elemath-backend-py/ecosystem.config.js
+++ Elemath-backend-py/ecosystem.config.js
@@ -3,7 +3,11 @@ module.exports = {
     {
       name: "fastapi-app",
       script: "venv/bin/python",
+<<<<<<< HEAD
       args: "-m uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4 --no-reload",
+=======
+      args: "main:app --host 0.0.0.0 --port 8001 --reload",
+>>>>>>> 243e9c6a (change)
       cwd: "/root/Elemath-full-project/Elemath-backend-py",
       interpreter: "none",
       autorestart: true,
@@ -15,3 +19,4 @@ module.exports = {
     }
   ]
 };
+
--- Elemath-backend-py/start.sh
+++ Elemath-backend-py/start.sh
@@ -7,4 +7,5 @@ source /root/Elemath-full-project/Elemath-backend-py/venv/bin/activate
 cd /root/Elemath-full-project/Elemath-backend-py
 
 # Run Uvicorn in production mode (no --reload)
+
 uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4
