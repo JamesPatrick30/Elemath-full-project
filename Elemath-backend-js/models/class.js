88ce28be7ed8b89b677bd931e835b6ce24085165
Jamespatrick30 <mrsniper1388@gmail.com>
2025-10-17 13:54:54 +0800

    grade change

--- Elemath-backend-js/models/class.js
+++ Elemath-backend-js/models/class.js
@@ -14,7 +14,11 @@ const classSchema = new mongoose.Schema({
   studentIds: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Student'
-  }]
+  }],
+  gradeLevel: {
+    type: String,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('Class', classSchema);
